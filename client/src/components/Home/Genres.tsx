996e57aa90aa7812d389fdf83d32c723b3661eaf
J.Boughattas <boughattasjassem@outlook.com>
2022-11-06 20:01:44 +0100

    seperate logic

---
+++ client/src/components/Home/Genres.tsx
@@ -0,0 +1,57 @@
+import {
+  Button,
+  Flex,
+  Spinner,
+  Text,
+  Box,
+  Wrap,
+  Heading,
+} from "@chakra-ui/react";
+
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { getAllGenres } from "../../api/genres";
+
+const Genres = () => {
+  const navigate = useNavigate();
+
+  const {
+    data: genres,
+    isError,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["genres"],
+    queryFn: getAllGenres,
+  });
+  return (
+    <>
+      <Flex maxW="90rem" mx="auto" pt="5rem">
+        {isLoading ? (
+          <Box py="1rem" mx="auto">
+            <Spinner size="md" />
+          </Box>
+        ) : null}
+        {isError ? <Text mx="auto">failed</Text> : null}
+        {isSuccess ? (
+          <Wrap py="1rem" mx="auto" justify="center">
+            <Button variant="ghost" onClick={() => navigate(`/`)}>
+              All
+            </Button>
+            {genres.map((genre) => (
+              <Button
+                key={genre.id}
+                variant="ghost"
+                onClick={() => navigate(`/${genre.title}`)}
+              >
+                {genre.title}
+              </Button>
+            ))}
+          </Wrap>
+        ) : null}
+      </Flex>
+    </>
+  );
+};
+
+export default Genres;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,50 +1,10 @@
-import { Button, Flex, Spinner, Text, Box, Wrap } from "@chakra-ui/react";
-
-import { useQuery } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import { getAllGenres } from "../../api/genres";
-
+import Genres from "../../components/Home/Genres";
 import Movies from "../../components/Home/Movies";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const {
-    data: genres,
-    isError,
-    isLoading,
-    isSuccess,
-  } = useQuery({
-    queryKey: ["genres"],
-    queryFn: getAllGenres,
-  });
-
   return (
     <>
-      <Flex maxW="90rem" mx="auto" pt="5rem">
-        {isLoading ? (
-          <Box py="1rem" mx="auto">
-            <Spinner size="md" />
-          </Box>
-        ) : null}
-        {isError ? <Text mx="auto">failed</Text> : null}
-        {isSuccess ? (
-          <Wrap py="1rem" mx="auto" justify="center">
-            <Button variant="ghost" onClick={() => navigate(`/`)}>
-              All
-            </Button>
-            {genres.map((genre) => (
-              <Button
-                key={genre.id}
-                variant="ghost"
-                onClick={() => navigate(`/${genre.title}`)}
-              >
-                {genre.title}
-              </Button>
-            ))}
-          </Wrap>
-        ) : null}
-      </Flex>
+      <Genres />
       <Movies />
     </>
   );
