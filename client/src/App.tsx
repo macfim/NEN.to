4769540bdec7c2670c8d69bc7e37affa1f5bda44
J.Boughattas <boughattasjassem@outlook.com>
2022-11-09 00:51:12 +0100

    implement code splitting for main components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useToast } from "@chakra-ui/react";
 
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+const NavBar = lazy(() => import("./components/NavBar"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Register = lazy(() => import("./pages/Register/Register"));
 
 import useLocalStorage from "./hooks/useLocalStorage";
 
@@ -20,25 +21,38 @@ const App = () => {
 
   return (
     <toastContext.Provider value={toast}>
-      <NavBar
-        token={token}
-        userUsername={userUsername}
-        setToken={setToken}
-        setUserUsername={setUserUsername}
-      />
+      <Suspense>
+        <NavBar
+          token={token}
+          userUsername={userUsername}
+          setToken={setToken}
+          setUserUsername={setUserUsername}
+        />
+      </Suspense>
       <Routes>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={
+            <Suspense>
+              <Home />
+            </Suspense>
+          }
+        />
         <Route path=":genre" element={<Home />} />
         <Route
           path="auth/login"
           element={
-            <Login setToken={setToken} setUserUsername={setUserUsername} />
+            <Suspense>
+              <Login setToken={setToken} setUserUsername={setUserUsername} />
+            </Suspense>
           }
         />
         <Route
           path="auth/register"
           element={
-            <Register setToken={setToken} setUserUsername={setUserUsername} />
+            <Suspense>
+              <Register setToken={setToken} setUserUsername={setUserUsername} />
+            </Suspense>
           }
         />
       </Routes>
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import Genres from "../../components/Home/Genres";
-import Movies from "../../components/Home/Movies";
+import { lazy, Suspense } from "react";
+
+const Genres = lazy(() => import("../../components/Home/Genres"));
+const Movies = lazy(() => import("../../components/Home/Movies"));
 
 const Home = () => {
   return (
     <>
-      <Genres />
-      <Movies />
+      <Suspense>
+        <Genres />
+        <Movies />
+      </Suspense>
     </>
   );
 };
