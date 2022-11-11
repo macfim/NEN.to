703d1b9df54f08ec3d4f5b1dbb4020a47b22b1b5
J.Boughattas <boughattasjassem@outlook.com>
2022-11-11 18:07:20 +0100

    rebuild ui

--- server/build/assets/genres.dfff8746.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index.ea771f0f.js";const t=async()=>(await e.get("/genres")).data;export{t as g};
---
+++ server/build/assets/genres.facc5978.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index.e562713a.js";const t=async()=>(await e.get("/genres")).data;export{t as g};
